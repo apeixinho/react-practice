f622d17cfa64d3fcaf496edc8f1cf30d29bf1aac
nunisa <monjit.nunisa@gmail.com>
2017-03-20 14:34:51 +0530

    Changes commit

--- webpack-demo/src/app/index.js
+++ webpack-demo/src/app/index.js
@@ -1,7 +1,9 @@
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
 import './stylesheets/app.css';
 import { monjit } from './components/test';
 
 console.info('In the index!', monjit);
 const index = 'This is index file.';
-document.getElementById('app').appendChild(monjit[0]);
+$('#app').append('<h1>Webpack Bundling</h1>');
+$('#app').append(monjit[0]);
